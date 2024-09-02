f5cae3dbf500d99deaa2cbd2c8fe43726f89170d
Michael Bestas <mkbestas@lineageos.org>
2024-09-02 12:28:54 +0530

    surya: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: shoya0x00 <shoya0x00@users.noreply.github.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2020-2023 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -55,9 +55,19 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/camera/components/com.qti.node.watermark.so)
-            grep -q "libpiex_shim.so" "${2}" || ${PATCHELF} --add-needed "libpiex_shim.so" "${2}"
+            [ "$2" = "" ] && return 0
+            grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
+            ;;
+        *)
+            return 1
             ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # Initialize the helper
