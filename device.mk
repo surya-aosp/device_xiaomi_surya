227dfe39e68d99fe19540fa5dd56da148b3f918c
Cyber Knight <cyberknight755@gmail.com>
2024-09-25 16:24:31 +0800

    surya: Keep building fingerprint HIDL interface lib
    - To retain compat with fingerprint.fpc.sm6150.so.
    
    Change-Id: I31cd006337b83456ce2142f1be99536c2419f784
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: shoya0x00 <shoya0x00@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.biometrics.fingerprint-service.xiaomi \
     com.fingerprints.extension@1.0.vendor \
     libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
