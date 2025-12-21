c2f323029997037ea9c5826330e9ad485521860e
shoya0x00 <shoya0x00@gmail.com>
2025-12-21 15:14:37 +0100

    surya: Import Signing keys

--- device.mk
+++ device.mk
@@ -22,6 +22,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # GcamGo
 $(call inherit-product, vendor/gcgop/config.mk)
 
+# Priv Keys
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
